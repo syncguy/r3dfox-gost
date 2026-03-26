ec9c27792f505c7d0f7c14c2e63b5cb66df4e0c1
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 10:03:59 -0500

    disable-data-reporting-at-compile-time.patch from Librewolf

--- python/mach/mach/telemetry.py
+++ python/mach/mach/telemetry.py
@@ -82,10 +82,7 @@ def is_applicable_telemetry_environment():
 
 
 def is_telemetry_enabled(settings):
-    if os.environ.get("DISABLE_TELEMETRY") == "1":
-        return False
-
-    return settings.mach_telemetry.is_enabled
+    return False
 
 
 def arcrc_path():
