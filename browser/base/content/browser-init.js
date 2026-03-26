9ff99e479f0a5fe838dc86447af6596d54d7c91e
the_r3dacted <k4sum1@eclipse.cx>
2026-03-26 10:10:12 -0500

    hide-passwordmgr.patch from Librewolf

--- browser/base/content/browser-init.js
+++ browser/base/content/browser-init.js
@@ -494,6 +494,13 @@ var gBrowserInit = {
       }
     }
 
+    if (Services.prefs.getBoolPref("librewolf.hidePasswdmgr", false)) {
+      PanelMultiView.getViewNode(
+        document,
+        "appMenu-passwords-button"
+      )?.remove();
+    }
+
     this._loadHandled = true;
   },
 
--- browser/components/preferences/privacy.js
+++ browser/components/preferences/privacy.js
@@ -768,6 +768,10 @@ var gPrivacyPane = {
 
     this.initWebAuthn();
 
+    if (Services.prefs.getBoolPref("librewolf.hidePasswdmgr", false)) {
+      document.getElementById("passwordsGroup")?.remove();
+    }
+
     // Notify observers that the UI is now ready
     Services.obs.notifyObservers(window, "privacy-pane-loaded");
   },
