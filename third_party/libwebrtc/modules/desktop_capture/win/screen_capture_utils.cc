0c2078653c9b0e6cffa10e6d5028548a7aa57b8e
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:15:06 -0500

    Bug 1837667 - avoid calling GetDpiForMonitor on Windows < 8.1;r=mjf,webrtc-reviewers,pehrsons    2023-06-21 00:31:18    752c94df7ee29eb74c72f274bd86d167c54e8c03

--- third_party/libwebrtc/modules/desktop_capture/win/screen_capture_utils.cc
+++ third_party/libwebrtc/modules/desktop_capture/win/screen_capture_utils.cc
@@ -10,8 +10,10 @@
 
 #include "modules/desktop_capture/win/screen_capture_utils.h"
 
+#include <libloaderapi.h>
 #include <shellscalingapi.h>
 #include <windows.h>
+#include <winnt.h>
 
 #include <cstddef>
 #include <optional>
@@ -21,12 +23,35 @@
 #include "modules/desktop_capture/desktop_capture_types.h"
 #include "modules/desktop_capture/desktop_capturer.h"
 #include "modules/desktop_capture/desktop_geometry.h"
+#include "mozilla/WindowsVersion.h"  // See Bug 1837647
 #include "rtc_base/checks.h"
 #include "rtc_base/logging.h"
 #include "rtc_base/string_utils.h"
 #include "rtc_base/win32.h"
 
 namespace webrtc {
+
+// See Bug 1837647 - upstream commit 60795e8c7a added a method using
+// ::GetDpiForMonitor which is not available on Win7 machines.  For Win7,
+// fail as to provoke it to get the system DPI.
+HRESULT TryGetDpiForMonitor(HMONITOR hmonitor,
+                            MONITOR_DPI_TYPE dpiType,
+                            UINT* dpiX,
+                            UINT* dpiY) {
+  static HRESULT (*plat_fn)(HMONITOR, MONITOR_DPI_TYPE, UINT*, UINT*);
+  // Grab a pointer to ::GetDpiForMonitor if that has been loaded.
+  // It is available in Windows 8.1 and up. Can we drop the version check? It
+  // would make upstreaming easier.
+  if (!plat_fn) {
+    if (auto* module = ::GetModuleHandle(L"Shcore.dll"); module) {
+      plat_fn = reinterpret_cast<decltype(plat_fn)>(
+          ::GetProcAddress(module, "GetDpiForMonitor"));
+    }
+  }
+  // Call the function we got or return a failure value in the case that
+  // we didn't manage to get a pointer to ::GetDpiForMonitor
+  return plat_fn ? ((*plat_fn)(hmonitor, dpiType, dpiX, dpiY)) : -1;
+}
 
 bool HasActiveDisplay() {
   DesktopCapturer::SourceList screens;
@@ -147,7 +172,7 @@ DesktopRect GetFullscreenRect() {
 DesktopVector GetDpiForMonitor(HMONITOR monitor) {
   UINT dpi_x, dpi_y;
   // MDT_EFFECTIVE_DPI includes the scale factor as well as the system DPI.
-  HRESULT hr = ::GetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &dpi_x, &dpi_y);
+  HRESULT hr = TryGetDpiForMonitor(monitor, MDT_EFFECTIVE_DPI, &dpi_x, &dpi_y);
   if (SUCCEEDED(hr)) {
     return {static_cast<INT>(dpi_x), static_cast<INT>(dpi_y)};
   }
