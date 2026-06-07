ef96604409773b64fc87b88595595bbfd066c76e
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:07:01 -0500

    Bug 1944724 p2 - Intercept GetProcAddress for ProcessPrng on Windows 7 for Widevine. r=yjuglaret,media-playback-reviewers,aosmond a=pascalc    2025-03-06 23:52:16    5f2ecf621d52ee9a1a5c1c3a480237a0d1a9883e

--- dom/media/gmp/ChromiumCDMAdapter.cpp
+++ dom/media/gmp/ChromiumCDMAdapter.cpp
@@ -19,6 +19,7 @@
 #ifdef XP_WIN
 #  include <strsafe.h>
 #  include <windows.h>
+#  include <bcrypt.h>
 
 #  include <unordered_map>
 #  include <vector>
@@ -221,6 +222,9 @@ typedef BOOL(WINAPI* SetThreadInformationFnPtr)(
 static WindowsDllInterceptor::FuncHookType<SetThreadInformationFnPtr>
     sOriginalSetThreadInformationFnPtr;
 
+static WindowsDllInterceptor::FuncHookType<decltype(&GetProcAddress)>
+    sOriginalGetProcAddressFnPtr;
+
 static std::unordered_map<std::wstring, std::wstring>* sDeviceNames = nullptr;
 
 DWORD WINAPI QueryDosDeviceWHook(LPCWSTR lpDeviceName, LPWSTR lpTargetPath,
@@ -310,6 +314,19 @@ BOOL WINAPI MozSetThreadInformation(
   return TRUE;
 }
 
+BOOL WINAPI MozProcessPrng(PBYTE pbData, SIZE_T cbData) {
+  return ::BCryptGenRandom(nullptr, pbData, cbData,
+                           BCRYPT_USE_SYSTEM_PREFERRED_RNG) == STATUS_SUCCESS;
+}
+
+FARPROC WINAPI MozGetProcAddress(HMODULE hModule, LPCSTR lpProcName) {
+  if (strcmp(lpProcName, "ProcessPrng") == 0) {
+    return reinterpret_cast<FARPROC>(&MozProcessPrng);
+  }
+
+  return sOriginalGetProcAddressFnPtr(hModule, lpProcName);
+}
+
 static void InitializeHooks() {
   static bool initialized = false;
   if (initialized) {
@@ -344,6 +361,11 @@ static void InitializeHooks() {
                                                   "SetThreadInformation",
                                                   &MozSetThreadInformation)) {
         GMP_LOG_WARNING("Failed to hook SetThreadInformation");
+      }
+
+      if (!sOriginalGetProcAddressFnPtr.Set(
+              sKernel32Intercept, "GetProcAddress", &MozGetProcAddress)) {
+        GMP_LOG_WARNING("Failed to hook GetProcAddress");
       }
     } else {
       GMP_LOG_WARNING(
