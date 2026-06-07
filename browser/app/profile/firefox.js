310a226d9af16efe50bc4adb1b2674b55eede095
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:08:49 -0500

    Bug 1403931: Enable USER_RESTRICTED on content process sandbox for Release. r=gcp    2024-11-22 00:36:08    6d00320a8a164629e01c05bac93a64fde489be64

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1588,7 +1588,7 @@ pref("browser.bookmarks.editDialog.maxRecentFolders", 7);
   // On windows these levels are:
   // See - security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
   // SetSecurityLevelForContentProcess() for what the different settings mean.
-  pref("security.sandbox.content.level", 9);
+  pref("security.sandbox.content.level", 7);
 
   // Pref controlling if messages relevant to sandbox violations are logged.
   pref("security.sandbox.logging.enabled", false);
