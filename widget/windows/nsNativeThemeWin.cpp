17f2b81c787284b4db2af06b6a42d54812bccbaa
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:06:07 -0500

    Bug 1967530 - Remove AssumeThemePartAndStateAreTransparent. r=win-reviewers,handyman    2025-05-21 04:06:06    f26eb3915cda1c09c464de2ccb18446080e7e3b2

--- widget/windows/nsNativeThemeWin.cpp
+++ widget/windows/nsNativeThemeWin.cpp
@@ -705,6 +705,15 @@ nsresult nsNativeThemeWin::GetThemePartAndState(nsIFrame* aFrame,
   }
 }
 
+static bool AssumeThemePartAndStateAreTransparent(int32_t aPart,
+                                                  int32_t aState) {
+  if (!LookAndFeel::GetInt(LookAndFeel::IntID::UseAccessibilityTheme) &&
+      aPart == MENU_POPUPITEM && aState == MBI_NORMAL) {
+    return true;
+  }
+  return false;
+}
+
 // When running with per-monitor DPI (on Win8.1+), and rendering on a display
 // with a different DPI setting from the system's default scaling, we need to
 // apply scaling to native-themed elements as the Windows theme APIs assume
@@ -744,6 +753,10 @@ void nsNativeThemeWin::DrawWidgetBackground(
   int32_t part, state;
   nsresult rv = GetThemePartAndState(aFrame, aAppearance, part, state);
   if (NS_FAILED(rv)) {
+    return;
+  }
+
+  if (AssumeThemePartAndStateAreTransparent(part, state)) {
     return;
   }
 
@@ -1359,6 +1372,10 @@ nsresult nsNativeThemeWin::ClassicDrawWidgetBackground(
   nsresult rv;
   MOZ_TRY(
       ClassicGetThemePartAndState(aFrame, aAppearance, part, state, focused));
+
+  if (AssumeThemePartAndStateAreTransparent(part, state)) {
+    return NS_OK;
+  }
 
   gfxFloat p2a = gfxFloat(aFrame->PresContext()->AppUnitsPerDevPixel());
   RECT widgetRect;
