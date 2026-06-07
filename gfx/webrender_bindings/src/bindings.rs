dfebaf8579fb91673497957937a5cc5cc2f1cf31
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:08:26 -0500

    Bug 1937638 - Fix popup windows when using layer compositor mode r=gfx-reviewers,lsalzman    2024-12-17 06:01:58    dae63526d8725b6a953ca127911aeca4646d6808

--- gfx/webrender_bindings/src/bindings.rs
+++ gfx/webrender_bindings/src/bindings.rs
@@ -2068,10 +2068,6 @@ pub extern "C" fn wr_window_new(
                 use_native_compositor,
             )),
         }
-    } else if use_layer_compositor {
-        CompositorConfig::Layer {
-            compositor: Box::new(WrLayerCompositor::new(compositor)),
-        }
     } else if use_native_compositor {
         CompositorConfig::Native {
             compositor: Box::new(WrCompositor(compositor)),
