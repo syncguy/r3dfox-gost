f67c60e0a9f883178048a39f0a2c9c70fdb48bf6
the_r3dacted <k4sum1@eclipse.cx>
2026-06-07 11:01:51 -0500

    Bug 2002998 - Stop calling into Dwm in order to hit test window buttons. r=win-reviewers,gstoll    2025-11-29 06:21:54    81a7119f574ddb72a078967c1225ffc937ad0a0b

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -4750,6 +4750,17 @@ bool nsWindow::ProcessMessageInternal(UINT msg, WPARAM& wParam, LPARAM& lParam,
     return true;
   }
 
+  // Glass hit testing w/custom transparent margins.
+  //
+  // FIXME(emilio): is this needed? We deal with titlebar buttons non-natively
+  // now.
+  LRESULT dwmHitResult;
+  if (mCustomNonClient &&
+      DwmDefWindowProc(mWnd, msg, wParam, lParam, &dwmHitResult)) {
+    *aRetValue = dwmHitResult;
+    return true;
+  }
+
   // The preference whether to use a different keyboard layout for each
   // window is cached, and updating it will not take effect until the
   // next restart. We read the preference here and not upon WM_ACTIVATE to make
@@ -4961,9 +4972,14 @@ bool nsWindow::ProcessMessageInternal(UINT msg, WPARAM& wParam, LPARAM& lParam,
         }
       }
 
-      /* If an nc client area margin has been moved, we are responsible
+      /*
+       * If an nc client area margin has been moved, we are responsible
        * for calculating where the resize margins are and returning the
-       * appropriate set of hit test constants. */
+       * appropriate set of hit test constants. DwmDefWindowProc (above)
+       * will handle hit testing on it's command buttons if we are on a
+       * composited desktop.
+       */
+
       if (!mCustomNonClient) {
         break;
       }
