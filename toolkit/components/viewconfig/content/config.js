9edbc8de187faf93612a8a75ec59b3259796690c
the_r3dacted <k4sum1@eclipse.cx>
2026-07-22 16:05:43 -0500

    Don't disable about:config context menu entries at all
    Can't be disabled when they're not supposed to be if they just don't get disabled.

--- toolkit/components/viewconfig/content/config.js
+++ toolkit/components/viewconfig/content/config.js
@@ -640,46 +640,6 @@ const configController = {
   onEvent: function onEvent(event) {},
 };
 
-function updateContextMenu() {
-  var lockCol = PREF_IS_LOCKED;
-  var typeCol = nsIPrefBranch.PREF_STRING;
-  var valueCol = "";
-  var copyDisabled = true;
-  var prefSelected = view.selection.currentIndex >= 0;
-
-  if (prefSelected) {
-    var prefRow = gPrefView[view.selection.currentIndex];
-    lockCol = prefRow.lockCol;
-    typeCol = prefRow.typeCol;
-    valueCol = prefRow.valueCol;
-    copyDisabled = false;
-  }
-
-  var copyPref = document.getElementById("copyPref");
-  copyPref.setAttribute("disabled", copyDisabled);
-
-  var copyName = document.getElementById("copyName");
-  copyName.setAttribute("disabled", copyDisabled);
-
-  var copyValue = document.getElementById("copyValue");
-  copyValue.setAttribute("disabled", copyDisabled);
-
-  var resetSelected = document.getElementById("resetSelected");
-  resetSelected.setAttribute("disabled", lockCol != PREF_IS_MODIFIED);
-
-  var canToggle = typeCol == nsIPrefBranch.PREF_BOOL && valueCol != "";
-  // indicates that a pref is locked or no pref is selected at all
-  var isLocked = lockCol == PREF_IS_LOCKED;
-
-  var modifySelected = document.getElementById("modifySelected");
-  modifySelected.setAttribute("disabled", isLocked);
-  modifySelected.hidden = canToggle;
-
-  var toggleSelected = document.getElementById("toggleSelected");
-  toggleSelected.setAttribute("disabled", isLocked);
-  toggleSelected.hidden = !canToggle;
-}
-
 function copyPref() {
   recordTelemetryOnce("Copy");
   var pref = gPrefView[view.selection.currentIndex];
